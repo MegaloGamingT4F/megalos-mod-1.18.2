7c442b0878ba8450ba7a642b3d3b3ed2e6b89297
MegaloGaming <v_megalo_v@hotmail.co.uk>
2022-01-14 09:58:41 +0000

    .json changes and icon.png were added.

--- src/main/java/net/megalogamingt4f/megalosmod/MegalosMod.java
+++ src/main/java/net/megalogamingt4f/megalosmod/MegalosMod.java
@@ -1,21 +1,19 @@
 package net.megalogamingt4f.megalosmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.megalogamingt4f.megalosmod.block.ModBlocks;
+import net.megalogamingt4f.megalosmod.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MegalosMod implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LogManager.getLogger("modid");
+    public static final String MOD_ID = "megalosmod";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
 
-        LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
     }
 }
